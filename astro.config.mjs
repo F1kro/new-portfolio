8e8836281e366b21ece2345ec3d62ea82fda8717
F1kro <mstok0369@gmail.com>
2026-02-18 01:42:49 +0800

    fixing deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel'; // 1. Import adapternya
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server', // <--- WAJIB TAMBAHKAN INI
+  output: 'server', // Ini yang bikin dia butuh adapter
+  adapter: vercel(), // 2. Pasang adapternya di sini
 });
